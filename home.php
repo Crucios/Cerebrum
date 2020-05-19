81402c42fb93998ea150c9fe43b2e1bcbe892153
henrywicaksono12 <61973389+henrywicaksono12@users.noreply.github.com>
2020-05-20 01:02:41 +0700

    Update home.php

--- home.php
+++ home.php
@@ -4,12 +4,9 @@
 	<title>Cerebrum</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type="text/css" href="website.css">
   <link rel="icon" href="assets/images/logo.png">
-	<script type="text/javascript" src="scripts/home.js"></script>
 </head>
 <body>
   <!-- Join Class Modal -->
@@ -88,5 +85,8 @@
      <img src="assets/images/user-icon.png">
    </a>
   </nav>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="scripts/home.js"></script>
 </body>
 </html>
