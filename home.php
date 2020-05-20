8690f8fe0818da148664fcc8580096563915c1df
henrywicaksono12 <61973389+henrywicaksono12@users.noreply.github.com>
2020-05-20 09:20:54 +0700

    Update navbar + logout

--- home.php
+++ home.php
@@ -1,3 +1,12 @@
+<?php  
+session_start();
+
+if(!isset($_SESSION["username"])){
+  header("Location: index.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,9 +26,9 @@
           <h5 class="modal-title">Join Class</h5>
         </div>
         <div class="modal-body form-group">
-            <p>Ask your teacher to tell you about the class code</p>
-            <label for="codeClass">Class Code</label>
-            <input name="codeClass" class="form-control" placeholder="ex: 8RsrGw64" id="joinClass_text" type="text">
+          <p>Ask your teacher to tell you about the class code</p>
+          <label for="codeClass">Class Code</label>
+          <input name="codeClass" class="form-control" placeholder="ex: 8RsrGw64" id="joinClass_text" type="text">
         </div>
         <div class="modal-footer" style="text-align: right;">
           <button type="button" class="btn" data-dismiss="modal">Cancel</button>
@@ -37,10 +46,10 @@
           <h5 class="modal-title">Create Class</h5>
         </div>
         <div class="modal-body form-group">
-            <label for="nameClass">Class Name (required)</label>
-            <input name="nameClass" class="form-control" placeholder="Mathematics D" id="nameClass_text" type="text">
-            <label for="descClass">Description (optional)</label>
-            <input name="descClass" class="form-control" placeholder="Mathematics Class of 2019/2020" id="descriptionClass_text" type="text">
+          <label for="nameClass">Class Name (required)</label>
+          <input name="nameClass" class="form-control" placeholder="Mathematics D" id="nameClass_text" type="text">
+          <label for="descClass">Description (optional)</label>
+          <input name="descClass" class="form-control" placeholder="Mathematics Class of 2019/2020" id="descriptionClass_text" type="text">
         </div>
         <div class="modal-footer" style="text-align: right;">
           <button type="button" class="btn" data-dismiss="modal">Cancel</button>
@@ -60,33 +69,40 @@
       <span class="navbar-toggler-icon"></span>
     </button>
 
-   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
      <ul class="nav navbar-nav">
-         <li class="nav-item active">
-           <form class="form-inline" style="margin-top: 5px;">
-             <input class="form-control mr-sm-2" type="search" placeholder="Search Classroom" aria-label="Search">
-             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-           </form>
-         </li>
-         <li class="nav-item dropdown">
-           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-             <img src="assets/images/plus.png" style="width:30px; height:30px;">
-             <span>Create or Join Class</span>
-           <a>
-           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-             <a class="dropdown-item" href="#" id="joinClass_button" data-toggle="modal" data-target="#joinClass_modal">Join Class</a>
-             <a class="dropdown-item" href="#" id="createClass_button" data-toggle="modal" data-target="#createClass_modal">Create Class</a>
-           </div>
-         </li>
+       <li class="nav-item active">
+         <form class="form-inline" style="margin-top: 5px;">
+           <input class="form-control mr-sm-2" type="search" placeholder="Search Classroom" aria-label="Search">
+           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+         </form>
+       </li>
+       <li class="nav-item dropdown">
+         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           <img src="assets/images/plus.png" style="width:30px; height:30px;">
+           <span>Create or Join Class</span>
+         </a>
+         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+           <a class="dropdown-item" href="#" id="joinClass_button" data-toggle="modal" data-target="#joinClass_modal">Join Class</a>
+           <a class="dropdown-item" href="#" id="createClass_button" data-toggle="modal" data-target="#createClass_modal">Create Class</a>
+         </div>
+       </li>
      </ul>
    </div>
-   <a class="navIcon" href="#">
-     <span>User Settings</span>
-     <img src="assets/images/user-icon.png">
-   </a>
-  </nav>
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="scripts/home.js"></script>
+   <div class="nav-item dropdown">
+     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <span style="color: lightgray;">User Settings</span>
+       <img src="assets/images/user-icon.png" style="width:30px; height:30px;">
+     </a>
+     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+       <a class="dropdown-item" href="#" id="joinClass_button">Change Password</a>
+       <a class="dropdown-item" href="#" id="createClass_button">Change Nickname</a>
+       <a class="dropdown-item" href="logout.php" id="createClass_button">Log Out</a>
+     </div>
+   </div>
+ </nav>
+ <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+ <script type="text/javascript" src="scripts/home.js"></script>
 </body>
 </html>
---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php  
+session_start();
+
+if(!isset($_SESSION["username"])){
+	header("Location: index.php");
+}
+
+session_unset();
+session_destroy();
+
+header("Location: index.php");
+
+?>
