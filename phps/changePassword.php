505250f1751fbe5693ec78d9e49771bb1d702fcf
Crucios <57003452+Crucios@users.noreply.github.com>
2020-05-20 17:02:33 +0700

    Change Password Worked

--- phps/changePassword.php
+++ phps/changePassword.php
@@ -1,38 +1,49 @@
 <?php
 
-require_once "phps/connect.php";
-session_start();
+require_once "connect.php";
 $errOldPass = $errNewPass = $errConfirmPass = "";
+$output = array('success' => false, 'errorOld' => null, 'errorNew' => null, 'errConfirm' => null, 'message' => null);
 
-if(isset($_POST["password"]) && isset($_POST["newPass"]) && isset($_POST["confirmPass"])){
+if(!empty($_POST["password"]) && !empty($_POST["newPass"]) && !empty($_POST["confirmPass"])){
+  session_start();
   $id = $_SESSION["id"];
   $password = test_input($_POST["password"]);
   $changePass = test_input($_POST["newPass"]);
   $confirmPass = test_input($_POST["confirmPass"]);
 	$query = mysqli_query($conn, "SELECT * FROM users WHERE id = $id AND password = '$password'");
 
+
 	if(mysqli_num_rows($query) > 0){
     if($changePass != $confirmPass){
-      $errNewPass = $errConfirmPass = "*Password does not match";
+      $output["errorNew"] = $output["errConfirm"] = "*Password does not match";
     }
     else{
-      $query = mysqli_query($conn, "SELECT * FROM users WHERE id = $id AND password = '$password'");
-    }
-	}else{
-		$errOldPass = "*Password incorrect";
-	}
+      $query2 = mysqli_query($conn, "UPDATE `users` SET `password` = '$confirmPass' WHERE `users`.`id` = $id");
+      if($query2){
+        $output["success"] = true;
+        $output["message"] = "Password successfully changed!";
+      }
+      else{
+        $output["message"] = "Password failed to change";
+      }
+  	}
+  }
+  else{
+    $output["errorOld"] = "*Password incorrect";
+  }
 }
 
-if(!isset($_POST["password"])){
-  $errOldPass = "*Password empty";
+if(empty($_POST["password"])){
+  $output["errorOld"] = "*Password empty";
 }
 
-if(!isset($_POST["newPass"])){
-  $errOldPass = "*New password empty";
+if(empty($_POST["newPass"])){
+  $output["errorNew"] = "*New password empty";
 }
 
-if(!isset($_POST["confirmPass"])){
-  $errOldPass = "*Confirm Password empty";
+if(empty($_POST["confirmPass"])){
+  $output["errConfirm"] = "*Confirm Password empty";
 }
 
+echo json_encode($output);
 ?>
