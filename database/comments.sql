0172dc26b91d973e8eefb48dcbd6840489b12ee3
henrywicaksono12 <61973389+henrywicaksono12@users.noreply.github.com>
2020-05-22 14:11:01 +0700

    Edit typo sql comments

--- database/comments.sql
+++ database/comments.sql
@@ -32,14 +32,14 @@ CREATE TABLE `comments` (
   `message` text NOT NULL,
   `timestamp` datetime NOT NULL,
   `id_users` int(11) NOT NULL,
-  `is_post` int(11) NOT NULL
+  `id_post` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`id`, `message`, `timestamp`, `id_users`, `is_post`) VALUES
+INSERT INTO `comments` (`id`, `message`, `timestamp`, `id_users`, `id_post`) VALUES
 (1, 'PRnya terlalu susah bu', '2020-05-05 00:10:50', 1, 1),
 (2, 'Terlalu hardcore pak', '0000-00-00 00:00:00', 1, 1);
 
