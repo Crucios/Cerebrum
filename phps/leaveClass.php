a036f165520fbb2c16b48b26a3b479df50338477
henrywicaksono12 <61973389+henrywicaksono12@users.noreply.github.com>
2020-05-27 19:54:21 +0700

    Revisi update bug

--- phps/leaveClass.php
+++ phps/leaveClass.php
@@ -10,15 +10,15 @@ $query = mysqli_query($conn, "UPDATE class_details SET status = 0 WHERE users_id
 
 if ($query) {
 	echo "Succesfully left $name!";
+	
+	unset($_SESSION["classname"]);
+	unset($_SESSION["creator"]);	
+	unset($_SESSION["description"]);
+	unset($_SESSION["code"]);
+	unset($_SESSION["role"]);
 }
 else{
 	echo "Failed to leave $name!";
 }
-
-unset($_SESSION["classname"]);
-unset($_SESSION["creator"]);	
-unset($_SESSION["description"]);
-unset($_SESSION["code"]);
-unset($_SESSION["role"]);
 
 ?>
